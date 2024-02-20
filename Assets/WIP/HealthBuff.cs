f0c67b0277532220cfea7b6f841d29b95b4e6c54
MatthiasssT <140664830+MatthiasssT@users.noreply.github.com>
2024-02-20 14:42:32 +1100

    UpgradeSystem
    - basics of the upgrade system implemented
    - basic health boost added
    - upgrade panel created

---
+++ Assets/WIP/HealthBuff.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "PowerUps")]
+public class HealthBuff : PowerUpSystem
+{
+    public float boostAmount;
+    public override void Apply(GameObject target)
+    {
+        target.GetComponent<Test>().maxHealth += (int)boostAmount;
+    }
+}
